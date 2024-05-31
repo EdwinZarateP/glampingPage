6666f618949516954907ed817cc59f53de31a308
EdwinZarateP <emzp1994@gmail.com>
2024-05-31 11:14:54 -0500

    link de menu a registro

--- src/Componentes/NavBar/index.tsx
+++ src/Componentes/NavBar/index.tsx
@@ -1,7 +1,8 @@
 import logo from '../../Imagenes/logoColor.png';
-import './estilos.css'
+import './estilos.css';
 import { useContext } from 'react';
-import { ContextoGlamping } from '../../Contexto/index'
+import { Link } from 'react-router-dom'; // Importa Link desde React Router
+import { ContextoGlamping } from '../../Contexto/index';
 import { BiWorld } from "react-icons/bi";
 import { FiMenu, FiSearch } from "react-icons/fi";
 import { AiOutlineUser } from "react-icons/ai";
@@ -9,64 +10,54 @@ import { GiCampingTent } from "react-icons/gi";
 import { TbFilterSearch } from "react-icons/tb";
 
 const NavBar = () => {
-  const almacenVar = useContext(ContextoGlamping)
+  const almacenVar = useContext(ContextoGlamping);
+
   return (
     <nav className='barNav'>
-
       {/* Parte Izquierda */}
-        <div className="contenedorizquierdo">
-          <div className="logoPrincipal">
-              <img src={logo} />
-          </div>
-
-          <div className="logoTienda">
-            <GiCampingTent />
-          </div>
-
+      <div className="contenedorizquierdo">
+        <div className="logoPrincipal">
+          <img src={logo} alt="Logo" />
         </div>
+        <div className="logoTienda">
+          <GiCampingTent />
+        </div>
+      </div>
 
-        {/* Parte media */}
-        <div className='contenedorMedio'>
-
-          <input
-            className='inputmedio'
-            type='search'
-            placeholder='Dónde'
-          />
-
+      {/* Parte media */}
+      <div className='contenedorMedio'>
+        <input
+          className='inputmedio'
+          type='search'
+          placeholder='Dónde'
+        />
         <div className='contenedorMedioBotones'>
-                 
           <button>Tiempo</button>
-                 
           <button>Quién cuantos</button>
-
         </div>
-
         <div className='lupaBusqueda'>
           <FiSearch />
         </div>
+      </div>
 
-        </div>
-
-        {/* Parte derecha */}
-        <div className='contenedorderecho'>
-          <p>Ofrece tu Glamping </p>
-            <BiWorld />
-        <div className='contenedorMenu'>
+      {/* Parte derecha */}
+      <div className='contenedorderecho'>
+        <p>Ofrece tu Glamping</p>
+        <BiWorld />
+        {/* Utiliza Link para envolver contenedorMenu */}
+        <Link to="/registro" className='contenedorMenu'>
           <FiMenu />
-          <AiOutlineUser/>
-        </div>
-                
-        </div>
+          <AiOutlineUser />
+        </Link>
+      </div>
 
-        {/* Parte derecha multifiltro */}
-        <div className='multifiltroNav' onClick={()=>almacenVar?.abrirFiltroAvanzado()}>
-        <TbFilterSearch />  
+      {/* Parte derecha multifiltro */}
+      <div className='multifiltroNav' onClick={() => almacenVar?.abrirFiltroAvanzado()}>
+        <TbFilterSearch />
+      </div>
 
-        </div>
-
-      </nav>
-    );
+    </nav>
+  );
 };
 
 export default NavBar;
