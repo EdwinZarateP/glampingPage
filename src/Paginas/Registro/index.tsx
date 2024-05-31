c2bac12068b1bf4234f9784f9487d05ed63f2412
EdwinZarateP <emzp1994@gmail.com>
2024-05-31 11:52:42 -0500

    confeti

--- src/Paginas/Registro/index.tsx
+++ src/Paginas/Registro/index.tsx
@@ -1,27 +1,53 @@
-import { GoogleLogin, CredentialResponse } from '@react-oauth/google'; 
-import { jwtDecode } from "jwt-decode";
+import { GoogleLogin, CredentialResponse } from '@react-oauth/google' 
+import { jwtDecode } from "jwt-decode"
+import { useState, useEffect  } from 'react'
+import Confetti from 'react-confetti'
+
 
 function Registro() {
+    const [usuario, setUsuario] = useState<string | null>(null)
+    const [mostrarConfeti, setMostrarConfeti] = useState(false)
+
+    const handleSuccess = (credentialResponse: CredentialResponse | undefined) => {
+        if (credentialResponse?.credential) {
+            const decoded: any = jwtDecode(credentialResponse.credential)
+            const nombreUsuario: string = decoded.name // Asumiendo que el nombre está en el token JWT
+            setUsuario(nombreUsuario)
+            setMostrarConfeti(true) // Mostrar el confeti al autenticarse correctamente
+        } else {
+            console.log('No se recibió el credencial')
+        }
+    }
+
+    useEffect(() => {
+        if (mostrarConfeti) {
+            const timeout = setTimeout(() => {
+                setMostrarConfeti(false) // Ocultar el confeti después de 3 segundos
+            }, 3000)
+
+            return () => clearTimeout(timeout)
+        }
+    }, [mostrarConfeti])
+
     return (
         <div>
-            <h1>Continuar con Google</h1>
+            <h2>Regístrate con tu cuenta de Google</h2>
             <span>
                 <GoogleLogin
-                    onSuccess={(credentialResponse: CredentialResponse | undefined) => {
-                        if (credentialResponse?.credential) {
-                            const decoded = jwtDecode(credentialResponse.credential)
-                            console.log(decoded);
-                        } else {
-                            console.log('No se recibió el credencial');
-                        }
-                    }}
+                    onSuccess={handleSuccess}
                     onError={() => {
-                        console.log('Login Failed');
+                        console.log('Login Failed')
                     }}
                 />
             </span>
+            {usuario && (
+                <div>
+                    <p>Bienvenido, {usuario}!</p>
+                    {mostrarConfeti && <Confetti width={window.innerWidth} height={window.innerHeight} />}
+                </div>
+            )}
         </div>
-    );
-};
+    )
+}
 
-export default Registro;
+export default Registro
